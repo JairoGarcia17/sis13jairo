ed26f795c69a55043e3e130244fffa90b6303057
pc <pc@172.16.17.146>
2017-03-29 13:36:06 -0600

    Iniciacion de el primer proyecto en Git

---
+++ src/sis13jairo/Sis13Jairo.java
@@ -0,0 +1,16 @@
+
+package sis13jairo;
+
+import java.util.Scanner;
+public class Sis13Jairo {
+
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+    }
+    
+}
